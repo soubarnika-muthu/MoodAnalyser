6a8a9d5eaf869e0db1353a3ef1039ca4850c980d
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-16 17:24:20 +0530

    [Soubarnika]UC1-Creating Method to Analyse Mood

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+   public  class MoodAnalyser
+    {
+        //method to Analyse mood passing Message as parameter 
+        public string AnalyseMood(string message)
+        {
+            message = message.ToLower();
+            if (message.Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MoodAnalyserProblem;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MoodAnalyser moodAnalyser;
+        [TestInitialize]
+        public void TestSetup()
+        {
+            moodAnalyser = new MoodAnalyser();
+        }
+        /// <summary>
+        /// TC 1.1 Given “I am in Sad Mood” message
+        // Should Return SAD
+        /// </summary>
+        [TestMethod]
+        public void ReturnSadTest()
+        {
+            ///AAA Methodology
+            //assign
+            string expected = "sad";
+            string message = "I am in SadMood";
+            //act
+            string actual = moodAnalyser.AnalyseMood(message);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        /// <summary>
+        /// TC 1.2 Given “I am in any mood” message
+        // Should Return happy
+        /// </summary>
+        [TestMethod]
+        public void ReturnHappyTest()
+        {
+            //assign
+            string expected = "happy";
+            string message = "I am in Any mood";
+            //act
+            string actual = moodAnalyser.AnalyseMood(message);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
